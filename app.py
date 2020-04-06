fab5b6bbad6ef080d5cda1de97d98b0844428f02
pierobartolo <piersilvio.debartolomeis@mail.polimi.it>
2020-04-07 00:54:39 +0200

    final structure

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def covid():
 
 
 scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(covid19.update_data, 'cron', hour=22, minute=53)  # Updating COVID Data 16 35
+scheduler.add_job(covid19.update_data, 'cron', hour=22, minute=58)  # Updating COVID Data 16 35
 scheduler.start()
 
 if __name__ == "__main__":
