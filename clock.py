7d2cf8b1c66f053cd50524173cb25dbc834d6914
pierobartolo <piersilvio.debartolomeis@mail.polimi.it>
2020-04-11 20:42:13 +0200

    Replaced background scheduler with blocking scheduler.

--- clock.py
+++ clock.py
@@ -1,7 +1,8 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 from utilities import covid19
 
-scheduler = BackgroundScheduler(daemon=True)
+scheduler = BlockingScheduler()
 #scheduler.add_job(covid19.update_data, 'cron', hour=16, minute=10)  # Updating COVID Data
 scheduler.add_job(covid19.update_data, 'interval', seconds=60)  # Updating COVID Data
+
 scheduler.start()
