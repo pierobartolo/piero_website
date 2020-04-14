4a2b6d67245ea841768c7e7fbfac7f290b50f434
pierobartolo <piersilvio.debartolomeis@mail.polimi.it>
2020-04-14 18:33:57 +0200

    added flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,6 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 talisman = Talisman(app, content_security_policy=Config.csp, content_security_policy_nonce_in=['script-src-elem', 'script-src'])
 app.config.from_object(Config)
-
+bootstrap = Bootstrap(app)
 
 from app import routes
