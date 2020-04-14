a275c6433633984785702d80abdf4f88e0ab3093
pierobartolo <piersilvio.debartolomeis@mail.polimi.it>
2020-04-14 18:33:30 +0200

    added flask_bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from flask_talisman import Talisman
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 talisman = Talisman(app, content_security_policy=Config.csp, content_security_policy_nonce_in=['script-src-elem', 'script-src'])
